05357e9078a973fce3b62255685cc491c9f9934f
Rafael Muijsert <rmuijsert@gmail.com>
2023-01-20 22:27:41 +0100

    init

--- blocks.h
+++ blocks.h
@@ -1,11 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
+#define BLOCK_DIRECTORY "~/.config/dwmblocks/"
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"🌡", BLOCK_DIRECTORY "cputemp",	10,			0},
+	{"🧠", BLOCK_DIRECTORY "memory",	60,			0},
+	{"", BLOCK_DIRECTORY "volume",		0,			10},
+	{"", BLOCK_DIRECTORY "battery",		60,			0},
+	{"", BLOCK_DIRECTORY "date",		30,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " \uE0B3 ";
 static unsigned int delimLen = 5;
